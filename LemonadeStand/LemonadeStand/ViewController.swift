c0f70718e21e835ab23ae3c72c53828822659a02
BM <bartosz@192-168-1-103.tpgi.com.au>
2015-06-16 23:40:21 +1000

    IBOutlets and IBActions added

--- LemonadeStand/LemonadeStand/ViewController.swift
+++ LemonadeStand/LemonadeStand/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var youHaveLabel: UILabel!
+    @IBOutlet weak var currentCashLabel: UILabel!
+    @IBOutlet weak var currentLemonsLabel: UILabel!
+    @IBOutlet weak var currentCubesLabel: UILabel!
+    @IBOutlet weak var orderLemonsLabel: UILabel!
+    @IBOutlet weak var orderCubesLabel: UILabel!
+    @IBOutlet weak var mixLemonsLabel: UILabel!
+    @IBOutlet weak var mixCubesLabel: UILabel!
+    
+    @IBOutlet weak var takeLemonsOrderButton: UIButton!
+    @IBOutlet weak var addLemonsOrderButton: UIButton!
+    @IBOutlet weak var takeCubesOrderButton: UIButton!
+    @IBOutlet weak var addCubesOrderButton: UIButton!
+    @IBOutlet weak var takeMixLemonsButton: UIButton!
+    @IBOutlet weak var addMixLemonsButton: UIButton!
+    @IBOutlet weak var takeMixCubesButton: UIButton!
+    @IBOutlet weak var addMixCubesButton: UIButton!
+    
+    @IBOutlet weak var startDayButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +40,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func takeLemonsOrderButtonPressed(sender: UIButton) {
+    }
+    @IBAction func addLemonsOrderButtonPressed(sender: UIButton) {
+    }
+    @IBAction func takeCubesOrderButtonPressed(sender: UIButton) {
+    }
+    @IBAction func addCubesOrderButtonPressed(sender: UIButton) {
+    }
+    @IBAction func takeLemonsMixButtonPressed(sender: UIButton) {
+    }
+    @IBAction func addLemonsMixButtonPressed(sender: UIButton) {
+    }
+    @IBAction func takeCubesMixButtonPressed(sender: UIButton) {
+    }
+    @IBAction func addCubesMixButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func startDayButtonPressed(sender: UIButton) {
+    }
+    
+    
+    
 }
 
